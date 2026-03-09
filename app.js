ed2cb5b754ff16e39a00cba0adb5abe0d251374e
Jorge Sandoval <jorgesagt5@gmail.com>
2026-03-08 18:35:53 -0700

    Se crearon los métodos get y post

--- app.js
+++ app.js
@@ -2,3 +2,38 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+
+app.use(express.json());
+
+let productos = [
+    { id: 1, nombre: "Laptop", precio: 15000 },
+    { id: 2, nombre: "Mouse", precio: 500 }
+];
+
+app.get("/productos", (req, res) => {
+    res.status(200).json(productos);
+});
+
+app.post("/productos", (req, res) => {
+    const {nombre, precio} = req.body;
+
+    if(!nombre || typeof(nombre) !== "string")
+        return res.status(400).json({error: "El nombre es invalido"});
+    
+    if(!precio || precio <= 0)
+        return res.status(400).json({error: "El precio es invalido"});
+
+    const nuevoProducto= {
+        id: productos.length + 1,
+        nombre,
+        precio
+    };
+
+    productos.push(nuevoProducto);
+    res.status(200).json(nuevoProducto);
+})
+
+
+app.listen(PORT, () => {
+    console.log('Servidor activo en http://localhost:' + PORT)
+})
